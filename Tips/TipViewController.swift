24a7861d97d5e4ecb0db8ffc4e6f1a39953696d3
pdoan <pdoan@zynga.com>
2015-04-27 09:39:50 -0700

    Change indent to 2 spaces

--- Tips/TipViewController.swift
+++ Tips/TipViewController.swift
@@ -9,92 +9,92 @@
 import UIKit
 
 class TipViewController: UIViewController {
-    @IBOutlet weak var billField: UITextField!
-    @IBOutlet weak var tipLabel: UILabel!
-    @IBOutlet weak var totalLabel: UILabel!
-    @IBOutlet weak var split2Label: UILabel!
-    @IBOutlet weak var split3Label: UILabel!
-    @IBOutlet weak var split4Label: UILabel!
-    @IBOutlet weak var tipControl: UISegmentedControl!
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        tipLabel.text = "$0.00"
-        totalLabel.text = "$0.00"
-        
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+  @IBOutlet weak var billField: UITextField!
+  @IBOutlet weak var tipLabel: UILabel!
+  @IBOutlet weak var totalLabel: UILabel!
+  @IBOutlet weak var split2Label: UILabel!
+  @IBOutlet weak var split3Label: UILabel!
+  @IBOutlet weak var split4Label: UILabel!
+  @IBOutlet weak var tipControl: UISegmentedControl!
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    tipLabel.text = "$0.00"
+    totalLabel.text = "$0.00"
+    
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func recalculateTip(){
+    var defaults = NSUserDefaults.standardUserDefaults();
+    var tipPercentages = defaults.mutableArrayValueForKey("tipPercentages");
+    var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex] as! Double;
+    
+    var billAmount = NSString(string:billField.text).doubleValue
+    var tip = billAmount * (tipPercentage/100)
+    var total = billAmount + tip
+    
+    tipLabel.text = String(format: "$%.2f", tip)
+    totalLabel.text = String(format: "$%.2f", total)
+    split2Label.text = String(format: "$%.2f", total/2)
+    split3Label.text = String(format: "$%.2f", total/3)
+    split4Label.text = String(format: "$%.2f", total/4)
+    
+  }
+  
+  @IBAction func onEditingChanged(sender: AnyObject) {
+    recalculateTip()
+    
+  }
+  @IBAction func onTap(sender: AnyObject) {
+    view.endEditing(true)
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    println("view will appear")
+    
+    var defaultTipPercentages = [15, 20, 25] as NSMutableArray;
+    
+    var defaults = NSUserDefaults.standardUserDefaults();
+    var tipPercentages = defaults.mutableArrayValueForKey("tipPercentages");
+    var tipDefaultIndex = defaults.integerForKey("tipDefaultIndex");
+    
+    if (tipPercentages.count == 0) {
+      defaults.setObject(defaultTipPercentages, forKey: "tipPercentages")
+      defaults.setInteger(1, forKey: "tipDefaultIndex")
+      defaults.synchronize()
+      
+      tipPercentages = defaults.mutableArrayValueForKey("tipPercentages");
+      tipDefaultIndex = defaults.integerForKey("tipDefaultIndex");
     }
     
-    func recalculateTip(){
-        var defaults = NSUserDefaults.standardUserDefaults();
-        var tipPercentages = defaults.mutableArrayValueForKey("tipPercentages");
-        var tipPercentage = tipPercentages[tipControl.selectedSegmentIndex] as! Double;
-        
-        var billAmount = NSString(string:billField.text).doubleValue
-        var tip = billAmount * (tipPercentage/100)
-        var total = billAmount + tip
-        
-        tipLabel.text = String(format: "$%.2f", tip)
-        totalLabel.text = String(format: "$%.2f", total)
-        split2Label.text = String(format: "$%.2f", total/2)
-        split3Label.text = String(format: "$%.2f", total/3)
-        split4Label.text = String(format: "$%.2f", total/4)
-        
+    for var index = 0; index < tipPercentages.count; ++index {
+      var temp = tipPercentages[index] as! Double;
+      tipControl.setTitle(String(format: "%.f%%", temp), forSegmentAtIndex: index)
     }
-
-    @IBAction func onEditingChanged(sender: AnyObject) {
-        recalculateTip()
-
-    }
-    @IBAction func onTap(sender: AnyObject) {
-        view.endEditing(true)
-    }
-    
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-        println("view will appear")
-        
-        var defaultTipPercentages = [15, 20, 25] as NSMutableArray;
-        
-        var defaults = NSUserDefaults.standardUserDefaults();
-        var tipPercentages = defaults.mutableArrayValueForKey("tipPercentages");
-        var tipDefaultIndex = defaults.integerForKey("tipDefaultIndex");
-        
-        if (tipPercentages.count == 0) {
-            defaults.setObject(defaultTipPercentages, forKey: "tipPercentages")
-            defaults.setInteger(1, forKey: "tipDefaultIndex")
-            defaults.synchronize()
-            
-            tipPercentages = defaults.mutableArrayValueForKey("tipPercentages");
-            tipDefaultIndex = defaults.integerForKey("tipDefaultIndex");
-        }
-        
-        for var index = 0; index < tipPercentages.count; ++index {
-            var temp = tipPercentages[index] as! Double;
-            tipControl.setTitle(String(format: "%.f%%", temp), forSegmentAtIndex: index)
-        }
-        tipControl.selectedSegmentIndex = tipDefaultIndex;
-        recalculateTip()
-    }
-    
-    override func viewDidAppear(animated: Bool) {
-        super.viewDidAppear(animated)
-        println("view did appear")
-    }
-    
-    override func viewWillDisappear(animated: Bool) {
-        super.viewWillDisappear(animated)
-        println("view will disappear")
-    }
-    
-    override func viewDidDisappear(animated: Bool) {
-        super.viewDidDisappear(animated)
-        println("view did disappear")
-    }
-    
+    tipControl.selectedSegmentIndex = tipDefaultIndex;
+    recalculateTip()
+  }
+  
+  override func viewDidAppear(animated: Bool) {
+    super.viewDidAppear(animated)
+    println("view did appear")
+  }
+  
+  override func viewWillDisappear(animated: Bool) {
+    super.viewWillDisappear(animated)
+    println("view will disappear")
+  }
+  
+  override func viewDidDisappear(animated: Bool) {
+    super.viewDidDisappear(animated)
+    println("view did disappear")
+  }
+  
 }
 
